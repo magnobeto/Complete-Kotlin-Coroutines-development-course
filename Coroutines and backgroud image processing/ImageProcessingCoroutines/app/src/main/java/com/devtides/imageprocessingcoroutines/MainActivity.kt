7ab9805e78505ca3863da8a7a41c1c2fa38c129e
Roberto Magno <roberto.magno@g.globo>
2021-05-20 15:08:41 -0300

    Baixando imagem em segundo plano

--- Coroutines and backgroud image processing/ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
+++ Coroutines and backgroud image processing/ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.devtides.imageprocessingcoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val IMAGE_URL =
+        "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) { getOriginalBitmap() }
+
+            val originalBitmap = originalDeferred.await()
+
+            loadImage(originalBitmap)
+        }
+    }
+
+    private fun getOriginalBitmap() =
+        URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    private fun loadImage(bmp: Bitmap) {
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
     }
 }
