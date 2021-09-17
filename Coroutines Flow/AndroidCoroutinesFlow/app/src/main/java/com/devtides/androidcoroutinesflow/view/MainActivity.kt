3402d40040388465646d4c1764423d47d9517425
Roberto Magno <roberto.magno@g.globo>
2021-09-17 17:48:12 -0300

    Using the flow

--- Coroutines Flow/AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/view/MainActivity.kt
+++ Coroutines Flow/AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/view/MainActivity.kt
@@ -32,7 +32,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun observeViewModel() {
         viewModel.newsArticles.observe(this, Observer { article ->
-
+            loading_view.visibility = View.GONE
+            newsList.visibility = View.VISIBLE
+            newsListAdapter.onAddNewsItem(article)
+            newsList.smoothScrollToPosition(0)
         })
     }
 }
--- Coroutines Flow/AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
+++ Coroutines Flow/AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -1,10 +1,11 @@
 package com.devtides.androidcoroutinesflow.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
 
 class ListViewModel: ViewModel() {
 
-    val newsArticles = MutableLiveData<String>()
+    val newsArticles = NewsRepository().getNewsArticles().asLiveData()
 
 }
